3e264b2f306952469399f07a8d62141e84562532
Florin Chelaru <florin.chelaru@gmail.com>
2022-03-14 10:38:15 +0200

    fetch json from API and display in the UI

--- express-ui-02/src/routes/index.ts
+++ express-ui-02/src/routes/index.ts
@@ -1,9 +1,20 @@
 import { Request, Response, Router } from 'express';
+import fetch from 'node-fetch';
+
+interface User {
+  id: string,
+  name: string,
+  email: string
+}
+
 const router = Router();
 
 /* GET home page. */
-router.get('/', (req: Request, res: Response) => {
-  res.render('index', { title: 'Express' });
+router.get('/', async (req: Request, res: Response) => {
+  const response = await fetch('http://192.168.64.4:3000/users');
+  const users = await response.json() as User[];
+//   console.log(users);
+  res.render('index', { title: 'Express', users: users });
 });
 
 export default router;
